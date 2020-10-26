33f6bb73ffed9fa22eceb0ac61af0eae28a60da8
Augustus Murphy <gusbmurphy@gmail.com>
2020-10-26 11:57:26 -0500

    Initial Commit

---
+++ Praxi/PraxiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PraxiApp.swift
+//  Praxi
+//
+//  Created by Augustus Murphy on 10/26/20.
+//
+
+import SwiftUI
+
+@main
+struct PraxiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
