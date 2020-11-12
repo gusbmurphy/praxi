3b9894705a991cfb92693d3ccea6c7fe0c5fd84a
Augustus Murphy <gusbmurphy@gmail.com>
2020-11-12 14:18:59 -0600

    Change ExerciseVariable to class with subclass

--- Praxi/Models/Exercise.swift
+++ Praxi/Models/Exercise.swift
@@ -14,45 +14,49 @@ struct Exercise {
     var description: String
     var variables: [ExerciseVariable]
     var areas: [String]
-    var records: [ExerciseRecord] = []
+//    var records: [ExerciseRecord] = []
     let id: UUID = UUID()
 }
 
-struct ExerciseVariable: Hashable {
+class ExerciseVariable {
     var name = ""
-    var type = ""
-    var setMembers: [String] = []
     
-    func getSetMembersString() -> String {
-        return setMembers.reduce("", {
-            $0 == "" ? $1 : $0 + ", " + $1
-        })
+    static func == (lhs: ExerciseVariable, rhs: ExerciseVariable) -> Bool {
+        return lhs.name == rhs.name
     }
 }
 
-struct ExerciseRecord: Hashable {
-    var date: Date
-    var variableValues: [String: Any] = [:]
-    var id: UUID = UUID()
-    
-    static func == (lhs: ExerciseRecord, rhs: ExerciseRecord) -> Bool {
-        return lhs.id == rhs.id
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
+class ExerciseRangeVariable<T>: ExerciseVariable {
+    var value: T? = nil
 }
 
-extension Exercise {
-    static let `default` = Self(
-        name: "Exercise",
-        image: Image("exerciseph"),
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit non massa in feugiat. Suspendisse fermentum, neque sed cursus convallis, purus dolor suscipit massa, sit amet egestas purus neque vel felis.",
-        variables: [
-            ExerciseVariable(name: "Tempo", type: "range"),
-            ExerciseVariable(name: "Scales", type: "set", setMembers: ["C", "F", "Bb", "Eb", "Ab", "Db", "Gb", "B", "E", "A", "D", "G"])
-        ],
-        areas: ["scales", "flexibility"]
-    )
+class ExerciseSetVariable<T>: ExerciseVariable {
+    var members: [T] = []
 }
+
+//struct ExerciseRecord: Hashable {
+//    var date: Date
+//    var variableValues: [String: Any] = [:]
+//    var id: UUID = UUID()
+//
+//    static func == (lhs: ExerciseRecord, rhs: ExerciseRecord) -> Bool {
+//        return lhs.id == rhs.id
+//    }
+//
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(id)
+//    }
+//}
+
+//extension Exercise {
+//    static let `default` = Self(
+//        name: "Exercise",
+//        image: Image("exerciseph"),
+//        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit non massa in feugiat. Suspendisse fermentum, neque sed cursus convallis, purus dolor suscipit massa, sit amet egestas purus neque vel felis.",
+//        variables: [
+//            ExerciseVariable(name: "Tempo", type: "range"),
+//            ExerciseVariable(name: "Scales", type: "set", setMembers: ["C", "F", "Bb", "Eb", "Ab", "Db", "Gb", "B", "E", "A", "D", "G"])
+//        ],
+//        areas: ["scales", "flexibility"]
+//    )
+//}
